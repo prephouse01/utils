779a7df7e32b6c9df2bc21bb98756613bd3f9b51
Iyoriobhe Wisdom Osenemendia <35114555+wisdomose@users.noreply.github.com>
2022-08-19 11:57:06 +0100

    docs: examples added

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { Question } from "../../src";
+import { Course, Question } from "../../src";
 
 const question = new Question({
   DB_URL: process.env.DB_URL as string,
@@ -7,21 +7,49 @@ const question = new Question({
   REVIEW_QUESTION_COST: Number(process.env.REVIEW_QUESTION_COST),
 });
 
-async function find(id: string) {
-  const q = await question.find({
-    id,
-  });
-  if (typeof q === typeof Error) {
-    console.log("error");
-    return;
-  }
-  console.log(q);
+const course = new Course({
+  DB_URL: process.env.DB_URL as string,
+  UPLOAD_QUESTION_COST: Number(process.env.UPLOAD_QUESTION_COST),
+  REVIEW_QUESTION_COST: Number(process.env.REVIEW_QUESTION_COST),
+});
+
+async function ops() {
+  // find course
+  await course
+    .findOne({
+      id: "62b09b5d8eab8da481309869",
+    })
+    .then((c) => {
+      console.log(c);
+      return;
+    })
+    .catch((e: any) => {
+      console.log(e.message);
+    });
+
+  // create course
+  await course
+    .create({
+      category: "secondary",
+      course: "mathematics",
+      examTypes: ["neco", "federal", "state"],
+      topics: ["set", "probability", "statistics"],
+    })
+    .then((c) => {
+      console.log(c);
+      return;
+    })
+    .catch((e: any) => {
+      console.log(e.message);
+    });
+
+  // find question
+  await question
+    .find({
+      id: ["62b4afe6e54bc2cafddf9cd0", "62b4b085e54bc2cafddf9cdd"],
+    })
+    .then((q) => console.log(q))
+    .catch((e: any) => console.log(e.message));
 }
 
-find("62b4afe6e54bc2cafddf9cd0");
-
-console.log("something else");
-
-find("62b4b085e54bc2cafddf9cdd");
-
-question.closeConnection();
+ops();
