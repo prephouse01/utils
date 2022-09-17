ee8a7154ed0087fdc117fab7c6908142b788ee28
Iyoriobhe Wisdom Osenemendia <35114555+wisdomose@users.noreply.github.com>
2022-09-17 03:04:40 +0100

    refactor: throw error for wrong inputs

--- src/utils/validateOptions.ts
+++ src/utils/validateOptions.ts
@@ -4,7 +4,14 @@ export function validateOption<T, K extends ZodTypeAny = AnyZodObject>(
   schema: K
 ) {
   return (input: T): T => {
-    const res = schema.parse(input);
-    return res as T;
+    try {
+      const res = schema.parse(input);
+      return res as T;
+    } catch (error: any) {
+      throw new Error (
+        error.issues.map(({ message }: { message: string }) => message).join() ??
+        "Invalid input"
+      );
+    }
   };
 }
